ec6144949c86c0e5b953997a7e7c341c9d4a7408
baizhen <galijiangzhi@163.com>
2024-03-27 17:45:29 +0800

    add data

--- utils.py
+++ utils.py
@@ -106,7 +106,7 @@ class CharsetMapper(object):
         all_chars = list(self.char_to_label.keys())
         valid_chars = []
         for c in all_chars:
-            if c in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ':
+            if c in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZཀཁགངཅཆཇཉཏཐདནཔཕབམཙཚཛཝཞཟའཡརལཤསཧཨ':
                 valid_chars.append(c)
         return ''.join(valid_chars)
 
